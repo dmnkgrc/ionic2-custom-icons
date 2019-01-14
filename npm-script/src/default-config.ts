9971b6a9f2166b6be68d0b628223ef56a72b2b4d
Dominik García <dominikgbertapelle@gmail.com>
2019-01-14 10:47:00 -0600

    use feather by default always

--- npm-script/src/default-config.ts
+++ npm-script/src/default-config.ts
@@ -1,11 +1,11 @@
-import {join} from 'path';
-import {DefaultConfig} from './config';
+import { join } from "path";
+import { DefaultConfig } from "./config";
 
 export const defaultConfig: DefaultConfig = {
-   templatePath: join(__dirname, '../scss/template.scss'),
-   fontTargetPath: '.tmp-custom-icons/fonts',
-   fontRelPath: '../assets/fonts/',
-   scssTargetPath: '.tmp-custom-icons/scss/',
-   scssRelPath: '../scss/${name}.scss',
-   debug: false
+  templatePath: join(__dirname, "../scss/template.scss"),
+  fontTargetPath: join(__dirname, "../../icons"),
+  fontRelPath: "../assets/fonts/",
+  scssTargetPath: ".tmp-custom-icons/scss/",
+  scssRelPath: "../scss/${name}.scss",
+  debug: false
 };
